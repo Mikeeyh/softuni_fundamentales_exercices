00b32bd541d01be08a962727595385584671179e
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-05-26 11:57:13 +0300

    edited

--- Exercices_1/maximum_multiple_for_loop.py
+++ Exercices_1/maximum_multiple_for_loop.py
@@ -3,7 +3,7 @@ boundary = int(input())
 number = 0
 
 for number in range(boundary, divisor - 1, -1):
-    if number == 0:   #it is not necessary to add this line because we're actually searching from the highest to lowest.
+    if number == 0:   #it is not necessary to add this line because we're searching from the highest to lowest.
         continue
     if number % divisor == 0:
         break
