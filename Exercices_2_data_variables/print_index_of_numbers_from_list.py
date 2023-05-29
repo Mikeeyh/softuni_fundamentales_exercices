179c70e48194136f1f72e6ff4d0786c97c75dc93
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-05-29 14:21:47 +0300

    adding

--- Exercices_2_data_variables/print_index_of_numbers_from_list.py
+++ Exercices_2_data_variables/print_index_of_numbers_from_list.py
@@ -11,16 +11,3 @@ for index in range(0, len(numbers)-1):
 
 print(list_of_found_indexes)
 
-# OR
-
-nums = [2, 7, 11, 15]
-target_number = 9
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        list_of_found_indexes = []
-        for index in range(0, len(numbers) - 1):
-            current_sum = int(nums[index]) + int(nums[index + 1])
-            if current_sum == target_number:
-                list_of_found_indexes.append(index)
-                list_of_found_indexes.append(index + 1)
-                return list_of_found_indexes
