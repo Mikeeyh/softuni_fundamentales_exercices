35e4a34f0d0e25e4289ea8c09237e784e4c3bae0
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-02 19:55:49 +0300

    Update characters_in_range.py

--- exercices_4_functions/characters_in_range.py
+++ exercices_4_functions/characters_in_range.py
@@ -1,18 +1,30 @@
-start_index = input()
-end_index = input()
-list_of_chars = []
-
-
 def ascii_converter(start, end):
-    start = ord(start_index)
-    end = ord(end_index)
+    list_of_chars = []
+    start = ord(start)
+    end = ord(end)
     for char in range(start + 1, end):
         list_of_chars.append(chr(char))
     return ' '.join(list_of_chars)
 
 
+start_index = input()
+end_index = input()
 result = ascii_converter(start_index, end_index)
 print(result)
 
+# OR -----------------
 
 
+def ascii_converter(start, end):
+    list_of_chars = []
+    start = ord(start)
+    end = ord(end)
+    for char in range(start + 1, end):
+        list_of_chars.append(chr(char))
+    return list_of_chars
+
+
+start_index = input()
+end_index = input()
+result = ascii_converter(start_index, end_index)
+print(' '.join(result))
