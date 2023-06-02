f9a082d8897be555800f766d61d69f0cbee7005f
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-02 19:50:24 +0300

    edited

--- exercices_4_functions/add_and_subtract.py
+++ exercices_4_functions/add_and_subtract.py
@@ -1,17 +1,36 @@
-first_number, second_number, third_number = int(input()), int(input()), int(input())
-
-
 def sum_numbers(first, second):
-    return first_number + second_number
-
-
-sum_of_two = sum_numbers(first_number, second_number)
+    return first + second
 
 
 def subtract(sum, third):
-    return sum_of_two - third_number
+    return sum - third
 
 
+first_number, second_number, third_number = int(input()), int(input()), int(input())
+sum_of_two = sum_numbers(first_number, second_number)
 result = subtract(sum_of_two, third_number)
 print(result)
+
+# OR -------------------------------------------------------------------------
+
+
+def sum_numbers(first, second):
+    return first + second
+
+
+def subtract(sum, third):
+    return sum - third
+
+
+def add_subtract(number_one, number_two, number_three):
+    sum_of_first_and_second_numbers = sum_numbers(number_one, number_two)
+    result = subtract(sum_of_first_and_second_numbers, number_three)
+    return result #or print(result)
+
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+
+print(add_subtract(first_number, second_number, third_number)) #or add_subtract(first_number, third_number)
 
--- exercices_4_functions/smallest_of_three_numbers.py
+++ exercices_4_functions/smallest_of_three_numbers.py
@@ -1,8 +1,21 @@
-
 def smallest_number(first, second, third):
     return min(first_number, second_number, third_number)
 
 
 first_number, second_number, third_number = int(input()), int(input()), int(input())
+
 min_number = smallest_number(first_number, second_number, third_number)
 print(min_number)
+
+# OR
+
+
+def smallest_number(some_numbers):
+    min_element = min(some_numbers)
+    return min_element
+
+
+first_number, second_number, third_number = int(input()), int(input()), int(input())
+numbers_list = [first_number, second_number, third_number]
+print(smallest_number(numbers_list))
+
