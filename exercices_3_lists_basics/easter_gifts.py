8c7973ad9ddc05c69da773bc155c012d187ddaa3
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-02 18:22:04 +0300

    Update easter_gifts.py

--- exercices_3_lists_basics/easter_gifts.py
+++ exercices_3_lists_basics/easter_gifts.py
@@ -1,14 +1,34 @@
-names_of_the_gifts = input().split()
-no_money = False
-my_gifts = []
+gifts = input().split()
+gifts_list = []
+command = input()
 
-while True:
+while command != "No Money":
+    command = command.split()
+    action = command[0]
+    gift = command[1]
+    if action == "OutOfStock":
+        for i in range(len(gifts)):
+            if gifts[i] == gift:
+                gifts[i] = "None"
+
+    elif action == "Required":
+        index = int(command[2])
+        if 0 <= index < len(gifts):
+            gifts[index] = gift
+
+    elif action == "JustInCase":
+        gifts[-1] = gift
+
     command = input()
 
-    if command == "No Money":
-        break
+# result = ' '.join([gift for gift in gifts if gift != "None"])
+# print(result)
 
-    my_gifts.append(names_of_the_gifts)
+# OR
 
-    if command == "OutOfStock":
-        pass
+for current_gift in gifts:
+    if current_gift != "None":
+        gifts_list.append(current_gift)
+print(' '.join(gifts_list))
+
+
