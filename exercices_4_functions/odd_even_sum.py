29f726d6ae419af8de5822db8e032541f5b60152
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-02 20:14:32 +0300

    Update odd_even_sum.py

--- exercices_4_functions/odd_even_sum.py
+++ exercices_4_functions/odd_even_sum.py
@@ -1,3 +1,24 @@
+def odd_even_numbers(some_number):
+    sum_of_odd_digits = 0
+    sum_of_even_digits = 0
+
+    for digit in some_number:
+        if int(digit) % 2 == 0:
+            sum_of_even_digits += int(digit)
+        else:
+            sum_of_odd_digits += int(digit)
+
+    return sum_of_odd_digits, sum_of_even_digits
+
+
+number = input()
+odd_digits, even_digits = odd_even_numbers(number)
+print(f'Odd sum = {odd_digits}, Even sum = {even_digits}')
+
+
+# OR --------------
+
+
 def string_to_list(string):
     list_of_numbers = []
     for i in range(len(string_of_numbers)):
@@ -19,4 +40,6 @@ def odd_even_filter(numbers):
 
 string_of_numbers = input()
 print(odd_even_filter(string_of_numbers))
+
+
 
