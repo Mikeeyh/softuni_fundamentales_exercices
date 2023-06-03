63162fd4d042a841e52f9dc8830235b214426c76
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-03 04:27:05 +0300

    Update loading_bar.py

--- exercices_4_functions/loading_bar.py
+++ exercices_4_functions/loading_bar.py
@@ -1,3 +1,19 @@
+def loading_bar(some_number):
+    if some_number == 100:
+        return f"100% Complete!\n[%%%%%%%%%%]"
+    else:
+        filled_percentage = number // 10
+        empty_percentage = 10 - filled_percentage
+        loading_line = "[" + "%" * filled_percentage + "." * empty_percentage + "]"
+        return f"{number}% {loading_line}\nStill loading..."
+
+
+number = int(input())
+print(loading_bar(number))
+
+# OR -------------------------
+
+
 def loading_bar(value):
     filled_percentage = number // 10
     empty_percentage = 10 - filled_percentage
@@ -15,6 +31,3 @@ number = int(input())
 total = loading_bar(number)
 print(total)
 
-# OR -------------------------
-
-def loading_bar(some_number)
