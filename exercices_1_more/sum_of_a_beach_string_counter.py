97c272dfc4be0a65ec147d25f8461e62a98fd1b9
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-05 20:13:40 +0300

    Update sum_of_a_beach_string_counter.py

--- exercices_1_more/sum_of_a_beach_string_counter.py
+++ exercices_1_more/sum_of_a_beach_string_counter.py
@@ -7,3 +7,10 @@ counter3 = string.count('fish')
 counter4 = string.count('sun')
 
 print(counter1 + counter2 + counter3 + counter4)
+
+print(counter1)
+print(counter2)
+print(counter3)
+print(counter4)
+
+print((input().lower()).count('water'))
