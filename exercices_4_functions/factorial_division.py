dd60dadb1f24eb3c2383ce77726cb60921825f09
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-05 12:36:54 +0300

    Update factorial_division.py

--- exercices_4_functions/factorial_division.py
+++ exercices_4_functions/factorial_division.py
@@ -1,21 +1,37 @@
-def factorial_division(num1, num2):
-    def factorial(n):
-        if n == 0:
-            return 1
-        else:
-            return n * factorial(n-1)
-
-    result1 = factorial(num_1)
-    result2 = factorial(num_2)
-    division = result1 / result2
-    formatted_division = "{:.2f}".format(division)
-
-    print(f"The factorial of {num1} is: {result1}")
-    print(f"The factorial of {num2} is: {result2}")
-    print(f"The division result is: {formatted_division}")
+def calculate_factorial(number):
+    for current_number in range(1, number):
+        number *= current_number
+    return number
 
 
-num_1 = int(input())
-num_2 = int(input())
-factorial_division(num_1, num_2)
+first_number = int(input())
+second_number = int(input())
+first_number_factorial = calculate_factorial(first_number)
+second_number_factorial = calculate_factorial(second_number)
+result = first_number_factorial / second_number_factorial
+print(f"{result:.2f}")
 
+# OR ---------------------------------------------------------------------------------
+
+#
+# def factorial_division(num1, num2):
+#     def factorial(n):
+#         if n == 0:
+#             return 1
+#         else:
+#             return n * factorial(n-1)
+#
+#     result1 = factorial(num_1)
+#     result2 = factorial(num_2)
+#     division = result1 / result2
+#     formatted_division = "{:.2f}".format(division)
+#
+#     print(f"The factorial of {num1} is: {result1}")
+#     print(f"The factorial of {num2} is: {result2}")
+#     print(f"The division result is: {formatted_division}")
+#
+#
+# num_1 = int(input())
+# num_2 = int(input())
+# factorial_division(num_1, num_2)
+#
