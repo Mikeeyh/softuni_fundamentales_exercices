5bc79ed89ec338631174cf4bcde458ade49cc097
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-05 19:46:15 +0300

    edited

---
+++ exercices_1_more/sum_of_a_beach_string_counter.py
@@ -0,0 +1,9 @@
+string = input()
+string = string.lower()
+
+counter1 = string.count('water')
+counter2 = string.count('sand')
+counter3 = string.count('fish')
+counter4 = string.count('sun')
+
+print(counter1 + counter2 + counter3 + counter4)
