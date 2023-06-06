33034a1f6e1f24f986920777723082ed213cd01f
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-06 11:17:56 +0300

    Update zeros_to_back.py

--- exercices_3_lists_more/zeros_to_back.py
+++ exercices_3_lists_more/zeros_to_back.py
@@ -16,3 +16,21 @@ result = list_of_non_zeros + list_of_zeros
 print(list_of_zeros)
 print(list_of_non_zeros)
 print(result)
+
+# OR ---------------------------------------------------------------------
+
+string = map(int, input().split(', '))
+list_of_zeros = []
+list_of_non_zeros = []
+
+for digit in string:
+    if digit == 0:
+        list_of_zeros.append(int(digit))
+    else:
+        list_of_non_zeros.append(int(digit))
+
+result = list_of_non_zeros + list_of_zeros
+print(list_of_zeros)
+print(list_of_non_zeros)
+print(result)
+
