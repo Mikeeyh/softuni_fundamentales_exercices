8e8787b67c867ca42d78c4b22154f7bb74c05130
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-06 11:14:09 +0300

    Create zeros_to_back.py

---
+++ exercices_3_lists_more/zeros_to_back.py
@@ -0,0 +1,18 @@
+string = input().split(', ')
+list_of_string = []
+list_of_zeros = []
+list_of_non_zeros = []
+
+for element in string:
+    list_of_string.append(int(element))
+
+for digit in list_of_string:
+    if digit == 0:
+        list_of_zeros.append(int(digit))
+    else:
+        list_of_non_zeros.append(int(digit))
+
+result = list_of_non_zeros + list_of_zeros
+print(list_of_zeros)
+print(list_of_non_zeros)
+print(result)
