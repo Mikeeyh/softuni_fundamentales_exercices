48b36220079d48537038aad623af7e3eaf024c2f
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-07 14:44:32 +0300

    Update list_manipulator.py

--- exercices_3_lists_more/list_manipulator.py
+++ exercices_3_lists_more/list_manipulator.py
@@ -1,1 +1,20 @@
+numbers = list(map(int, input().split()))
+command = input()
+first_list = []
+second_list = []
 
+while True:
+    if command == 'end':
+        break
+    command = command.split()
+    action = command[0]
+    number = int(command[1])
+
+    if action == "exchange":
+        if number < 0 or number >= len(numbers):
+            print("Invalid index")
+        first_list = numbers[:number + 1]
+        second_list = numbers[number + 1:]
+        print(second_list + first_list)
+
+    command = input()
