3e3cba93ce1b699f9183d230bdc21192a6d6e695
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-07 15:39:02 +0300

    Update list_manipulator.py

--- exercices_3_lists_more/list_manipulator.py
+++ exercices_3_lists_more/list_manipulator.py
@@ -8,14 +8,48 @@ while True:
         break
     command = command.split()
     action = command[0]
-    number = int(command[1])
+    second_action = command[1]
 
     if action == "exchange":
+        second_action = int(command[1])
+        number = second_action
         if number < 0 or number >= len(numbers):
             print("Invalid index")
         else:
             first_list = numbers[:number + 1]
             second_list = numbers[number + 1:]
             print(second_list + first_list)
+
+    if action == "max":
+        if second_action == "even":
+            filtered_list = [num for num in numbers if num % 2 == 0]
+            if not filtered_list:
+                print('No matches')
+            else:
+                max_num = max(filtered_list)
+                print(numbers.index(max_num))
+        elif second_action == "odd":
+            filtered_list = [num for num in numbers if num % 2 != 0]
+            if not filtered_list:
+                print('No matches')
+            else:
+                max_num = max(filtered_list)
+                print(numbers.index(max_num))
+
+    if action == "min":
+        if second_action == "even":
+            filtered_list = [num for num in numbers if num % 2 == 0]
+            if not filtered_list:
+                print('No matches')
+            else:
+                min_num = min(filtered_list)
+                print(numbers.index(min_num))
+        elif second_action == "odd":
+            filtered_list = [num for num in numbers if num % 2 != 0]
+            if not filtered_list:
+                print('No matches')
+            else:
+                min_num = min(filtered_list)
+                print(numbers.index(min_num))
 
     command = input()
