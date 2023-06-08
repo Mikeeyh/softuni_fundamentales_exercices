5cf97ea56f501efede1d021b6831a91a9627d74b
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-09 02:10:06 +0300

    adding

--- exercices_5_lists_advanced/office_chairs.py
+++ exercices_5_lists_advanced/office_chairs.py
@@ -1,11 +1,22 @@
 rooms = int(input())
 
+total_free_chairs = 0
+enough_chairs = True
+
 for room in range(rooms):
-    room_information = input().split()
-    chairs = room_information[0]
-    people = room_information[1]
+    room_information = input()
+    chairs, visitors = room_information.split()
+    chairs_count = len(chairs)
+    visitors_count = int(visitors)
 
-    chairs_in_room = [chair for chair in chairs]
+    if visitors_count > chairs_count:
+        enough_chairs = False
+        needed_chairs = visitors_count - chairs_count
+        print(f"{needed_chairs} more chairs needed in room {room + 1}")
+    else:
+        free_chairs = chairs_count - visitors_count
+        total_free_chairs += free_chairs
 
-    if people > len(chairs_in_room):
-        print(f"More chairs needed in room.")
+if enough_chairs:
+    print(f"Game On, {total_free_chairs} free chairs left")
+
