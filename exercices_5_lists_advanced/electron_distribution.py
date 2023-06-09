be01d930faecfe9b8fdf9e405948d74a427c5fa5
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-09 04:53:36 +0300

    Update electron_distribution.py

--- exercices_5_lists_advanced/electron_distribution.py
+++ exercices_5_lists_advanced/electron_distribution.py
@@ -1,6 +1,41 @@
-number = int(input())
+def fill_shells(electrons):
+    shells = []
+    shell_number = 1
 
-electron_list = [2 * electron ** 2 for electron in range(1, 10) if 2 * electron ** 2 < number]
-if sum(electron_list) > number:
-    electron_list[-1] = sum(electron_list) - number
-print(electron_list)
+    while electrons > 0:
+        adding_electrons = 2 * shell_number**2
+
+        if electrons >= adding_electrons:
+            shells.append(adding_electrons)
+            electrons -= adding_electrons
+        else:
+            shells.append(electrons)
+            electrons = 0
+        shell_number += 1
+    return shells
+
+
+num_electrons = int(input())
+filled_shells = fill_shells(num_electrons)
+print(filled_shells)
+
+# OR ----------------------------------------
+
+number_of_electrons = int(input())
+list_of_electrons = []
+current_index = 1
+
+while number_of_electrons > 0:
+
+    electrons = 2 * current_index ** 2
+
+    if number_of_electrons >= electrons:
+        list_of_electrons.append(electrons)
+        number_of_electrons -= electrons
+    else:
+        list_of_electrons.append(number_of_electrons)
+        number_of_electrons = 0
+
+    current_index += 1
+
+print(list_of_electrons)
