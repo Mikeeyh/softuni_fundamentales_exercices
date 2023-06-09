24a3d7e65dd38f787153bbd0a2497e1c80d86367
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-09 14:36:24 +0300

    Update number_classification.py

--- exercices_5_lists_advanced/number_classification.py
+++ exercices_5_lists_advanced/number_classification.py
@@ -1,4 +1,5 @@
 numbers = list(map(int, input().split(', ')))
+# numbers = [int(number) for number in input().split(',')]
 
 even = [number for number in numbers if number % 2 == 0]
 odd = [number for number in numbers if number % 2 != 0]
@@ -11,3 +12,16 @@ print('Even:', end=' '), print(*even, sep=", ")
 print('Odd:', end=' '), print(*odd, sep=", ")
 # OR print(f'Odd: {", ".join(map(str, odd))}')
 
+# OR ----------------------------------------------------------------
+
+numbers = input().split(', ')
+
+even = [number for number in numbers if int(number) % 2 == 0]
+odd = [number for number in numbers if int(number) % 2 != 0]
+positives = [number for number in numbers if int(number) >= 0]
+negatives = [number for number in numbers if int(number) < 0]
+
+print(f"Positive: {', '.join(positives)}")
+print(f"Negative: {', '.join(negatives)}")
+print(f"Even: {', '.join(even)}")
+print(f"Odd: {', '.join(odd)}")
