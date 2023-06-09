3fa80763f8360b9f4c4f2a6e580cdde154c27ee0
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-09 14:23:13 +0300

    Update word_filter.py

--- exercices_5_lists_advanced/word_filter.py
+++ exercices_5_lists_advanced/word_filter.py
@@ -1,4 +1,7 @@
 text = input().split()
 filtered_text = [letter for letter in text if len(letter) % 2 == 0]
-print(filtered_text)
 print('\n'.join(filtered_text))
+
+# OR ------------------------------------------------------
+
+print('\n'.join([word for word in input().split() if len(word) % 2 == 0]))
