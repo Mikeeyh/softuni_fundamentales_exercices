0045aaee48dbde407256acc0edc3500a365ad852
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-09 17:57:08 +0300

    Update group_of_10's.py

--- exercices_5_lists_advanced/group_of_10's.py
+++ exercices_5_lists_advanced/group_of_10's.py
@@ -1,19 +1,3 @@
-sequence = input().split(", ")
-groups = {}
-
-for number in sequence:
-    number = int(number)
-
-    group = (number // 10) * 10
-    if group not in groups:
-        groups[group] = []
-    groups[group].append(number)
-
-for group, sequence in groups.items():
-    print(f"Group of {group}'s: {sequence}")
-
-# OR-----------------------------------------------------
-
 numbers = [int(num) for num in input().split(', ')]
 current_group_of_numbers = 10
 
