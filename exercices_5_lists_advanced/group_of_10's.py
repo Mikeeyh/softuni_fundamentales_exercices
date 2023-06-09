51115ccd3edef6f82e9b2672a34dab617e96e6cd
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-09 17:50:38 +0300

    Update group_of_10's.py

--- exercices_5_lists_advanced/group_of_10's.py
+++ exercices_5_lists_advanced/group_of_10's.py
@@ -12,7 +12,19 @@ for number in sequence:
 for group, sequence in groups.items():
     print(f"Group of {group}'s: {sequence}")
 
+# OR-----------------------------------------------------
+
+numbers = [int(num) for num in input().split(', ')]
+current_group_of_numbers = 10
+
+while numbers: # numbers > 0
+
+    filtered_numbers_for_current_group = [number for number in numbers if number <= current_group_of_numbers]
+
+    print(f"Group of {current_group_of_numbers}'s: {filtered_numbers_for_current_group}")
+    current_group_of_numbers += 10
+
+    numbers = [number for number in numbers if number not in filtered_numbers_for_current_group]
 
 
-print((5 // 10) * 10)
 
