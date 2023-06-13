10145d57af18b6d496bfd548d0814eb9a4977b5d
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-13 18:42:24 +0300

    Update inventory.py

--- mid_exams/inventory.py
+++ mid_exams/inventory.py
@@ -12,7 +12,7 @@ while command != 'Craft!':
     elif action == 'Drop':
         if item in items:
             index = items.index(item)
-            items.pop(index)
+            items.pop(index) # we can do it without searching for the index and using pop / we can use remove
     elif action == 'Combine Items':
         item = item.split(':')
         old_item = item[0]
@@ -29,3 +29,33 @@ while command != 'Craft!':
     command = input()
 
 print(', '.join(items))
+
+# OR --------------------------------------------------------
+
+journal = input().split(", ")
+command = input()
+
+while command != "Craft!":
+    command_data = command.split(" - ")
+    action = command_data[0]
+    item = command_data[1]
+
+    if action == "Collect":
+        if item not in journal:
+            journal.append(item)
+    elif action == "Drop":
+        if item in journal:
+            journal.remove(item)
+    elif action == "Combine Items":
+        old_item, new_item = item.split(":")
+        if old_item in journal:
+            index = journal.index(old_item)
+            journal.insert(index + 1, new_item)
+    elif action == "Renew":
+        if item in journal:
+            journal.remove(item)
+            journal.append(item)
+
+    command = input()
+
+print(", ".join(journal))
