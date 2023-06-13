3e7c2d427dbb1e0b620cc670da532e6fd38e2ed5
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-13 12:27:03 +0300

    Update array_modifier.py

---
+++ mid_exams/array_modifier.py
@@ -0,0 +1,26 @@
+array_numbers = list(map(int, input().split()))
+command = input()
+
+while command != "end":
+    command = command.split()
+    action = command[0]
+
+    if action == "swap":
+        index_1 = int(command[1])
+        index_2 = int(command[2])
+        array_numbers[index_1], array_numbers[index_2] = array_numbers[index_2], array_numbers[index_1]
+
+    elif action == "multiply":
+        index_1 = int(command[1])
+        index_2 = int(command[2])
+        array_numbers[index_1] *= array_numbers[index_2]
+
+    elif action == "decrease":
+        array_numbers = [num - 1 for num in array_numbers]
+
+    command = input()
+
+# output = ", ".join([str(num) for num in array])
+# print(output)
+
+print(*array_numbers, sep=", ")
