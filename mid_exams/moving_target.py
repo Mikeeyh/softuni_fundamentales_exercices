2de48ddc2a5244b104ee1bff23c5bd43ae9790cb
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-15 12:35:00 +0300

    Create moving_target.py

---
+++ mid_exams/moving_target.py
@@ -0,0 +1,36 @@
+targets = list(map(int, input().split()))
+command = input()
+
+while command != 'End':
+    command = command.split()
+    action = command[0]
+
+    if action == 'Shoot':
+        index = int(command[1])
+        power = int(command[2])
+        if 0 <= index < len(targets):
+            targets[index] -= power
+            if targets[index] <= 0:
+                targets.pop(index)
+
+    elif action == 'Add':
+        index = int(command[1])
+        value = int(command[2])
+        if 0 <= index < len(targets):
+            targets.insert(index, value)
+        else:
+            print(f"Invalid placement!")
+
+    elif action == 'Strike':
+        index = int(command[1])
+        radius = int(command[2])
+        start_index = index - radius
+        end_index = index + radius
+        if start_index >= 0 and end_index < len(targets):
+            del targets[start_index:end_index+1]
+        else:
+            print("Strike missed!")
+
+    command = input()
+
+print(*targets, sep="|")
