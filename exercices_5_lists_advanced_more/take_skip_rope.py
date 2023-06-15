9978b2d9522d3d340f6e37f8eb64ffde2854a3dd
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-15 22:53:07 +0300

    Update take_skip_rope.py

--- exercices_5_lists_advanced_more/take_skip_rope.py
+++ exercices_5_lists_advanced_more/take_skip_rope.py
@@ -14,3 +14,31 @@ for take, skip in zip(take_list, skip_list):
     non_numbers = non_numbers[take + skip:]
 
 print(result)
+
+# OR -----------------------------------------------------------------------------
+
+import re
+
+def extract_hidden_message(string):
+    numbers = [int(digit) for digit in string if digit.isdigit()]
+    non_numbers = re.findall('[^0-9]', string)
+
+    take_list = [numbers[i] for i in range(len(numbers)) if i % 2 == 0]
+    skip_list = [numbers[i] for i in range(len(numbers)) if i % 2 != 0]
+
+    result = ''
+    skip_count = 0
+
+    for take, skip in zip(take_list, skip_list):
+        result += ''.join(non_numbers[:take])
+        non_numbers = non_numbers[take + skip:]
+        skip_count += skip
+
+    result += ''.join(non_numbers[skip_count:])
+
+    return result
+
+
+input_string = input()
+hidden_message = extract_hidden_message(input_string)
+print(hidden_message)
