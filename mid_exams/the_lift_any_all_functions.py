e22cadd4ebc5584d8cc9309bb2209090ef15e85f
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-18 12:58:36 +0300

    Update the_lift_any_all_functions.py

--- mid_exams/the_lift_any_all_functions.py
+++ mid_exams/the_lift_any_all_functions.py
@@ -1,3 +1,5 @@
+from tkinter.tix import MAX
+
 people = int(input())
 wagons = list(map(int, input().split()))
 
@@ -19,3 +21,25 @@ elif people == 0 and all(seats == 4 for seats in wagons):
 
 # OR -----------------------------------------------------
 
+people = int(input())
+lift_cabins = [int(number) for number in input().split()]
+MAX_SPACES = 4
+
+for current_cabin in range(len(lift_cabins)):
+    free_spaces = MAX_SPACES - lift_cabins[current_cabin]
+
+    if people >= free_spaces:
+        lift_cabins[current_cabin] += free_spaces
+    else:
+        lift_cabins[current_cabin] += people
+
+    people -= free_spaces
+
+    if people <= 0 and (current_cabin != len(lift_cabins) - 1 or lift_cabins[current_cabin] < MAX_SPACES):
+        print("The lift has empty spots!")
+        break
+else:
+    if people > 0:
+        print(f"There isn't enough space! {people} people in a queue!")
+
+print(*lift_cabins)
