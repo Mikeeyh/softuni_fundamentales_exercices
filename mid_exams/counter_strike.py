fe2f81701e9810b48b810281a1cf2d3db7e1aa9c
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-06-18 11:32:55 +0300

    Update counter_strike.py

--- mid_exams/counter_strike.py
+++ mid_exams/counter_strike.py
@@ -20,3 +20,26 @@ while command != 'End of battle':
 
 if not not_success:
     print(f"Won battles: {battle_won}. Energy left: {initial_energy}")
+
+# OR ------------------------------------------------------------------
+
+initial_energy = int(input())
+command = input()
+battle_won = 0
+
+while command != 'End of battle':
+    energy_needed = int(command)
+
+    if energy_needed <= initial_energy:
+        battle_won += 1
+        initial_energy -= energy_needed
+        if battle_won % 3 == 0:
+            initial_energy += battle_won
+    else:
+        print(f"Not enough energy! Game ends with {battle_won} won battles and {initial_energy} energy")
+        break
+
+    command = input()
+
+else:
+    print(f"Won battles: {battle_won}. Energy left: {initial_energy}")
