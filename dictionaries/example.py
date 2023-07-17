6c5d3b33e58fe7d58e7fb34e309460172e78b0d0
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-07-18 00:19:59 +0300

    Create example.py

---
+++ dictionaries/example.py
@@ -0,0 +1,39 @@
+keys = ["name", "age", "major"]
+values = ["Ivan", 25, "Computer Science"]
+
+student = {}
+
+for i in range(len(keys)):
+    key = keys[i]
+    value = values[i]
+    student[key] = value
+
+print(student)
+
+# OR ------------------------------------------------------------
+
+student = dict(name="Ivan", age=25, major="Computer Science")
+print(student)
+
+student = dict([("name", "Ivan"), ("age", 25), ("major", "Computer Science")])
+print(student)
+
+# OR -------------------------------------------------------------
+
+keys = ["name", "age", "major"]
+values = ["Ivan", 25, "Computer Science"]
+
+student = dict(zip(keys, values))
+
+print(student)
+
+# OR -------------------------------------------------------------
+
+student = {
+    "name": "Ivan",
+    "age": 25,
+    "major": "Computer Science",
+}
+
+print(student)
+print(student['name'])
