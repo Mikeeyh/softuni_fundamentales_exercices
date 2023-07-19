d6807eddc7b226ca12639d00a587375d33d4c9ac
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-07-19 12:34:41 +0300

    Update miner_task.py

--- exercices_7_dictionaries/miner_task.py
+++ exercices_7_dictionaries/miner_task.py
@@ -16,3 +16,21 @@ while True:
 
 for resource, quantity_of_the_given_resource in stock.items():
     print(f"{resource} -> {quantity_of_the_given_resource}")
+
+# OR --------------------------------------------------------------------------
+
+resources = {}
+
+while True:
+    current_resource = input()
+    if current_resource == "stop":
+        break
+
+    current_quantity = int(input())
+
+    if current_resource not in resources.keys():
+        resources[current_resource] = 0
+    resources[current_resource] += current_quantity
+
+for resource, quantity in resources.items():
+    print(f"{resource} -> {quantity}")
