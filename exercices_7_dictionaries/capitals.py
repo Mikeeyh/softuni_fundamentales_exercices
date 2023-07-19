5f64f6fe38fec423313341378d91880c4eb0c846
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-07-19 13:37:09 +0300

    add

--- exercices_7_dictionaries/capitals.py
+++ exercices_7_dictionaries/capitals.py
@@ -5,3 +5,12 @@ countries_capitals = {key: value for (key, value) in zip(first_line, second_line
 
 for country, capital in countries_capitals.items():
     print(f"{country} -> {capital}")
+
+# OR ------------------------------------------------------------------------------
+
+countries = input().split(", ")
+capitals = input().split(", ")
+
+information = {countries[index]: capitals[index] for index in range(len(countries))}
+for country, capital in countries_capitals.items():
+    print(f"{country} -> {capital}")
--- exercices_7_dictionaries/phonebook.py
+++ exercices_7_dictionaries/phonebook.py
@@ -1,11 +1,16 @@
 phonebook = {}
 
-data = input()
-
 while True:
-    if data.isdigit():
-        data = int(data)
+    entry = input()
+    if "-" not in entry:
         break
-    data = input()
 
-print(data)
+    name, phone_number = entry.split("-")
+    phonebook[name] = phone_number
+
+for search in range(int(entry)):
+    searched_name = input()
+    if searched_name in phonebook.keys():
+        print(f"{searched_name} -> {phonebook[searched_name]}")
+    else:
+        print(f"Contact {searched_name} does not exist.")
