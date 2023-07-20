82a6e373186c61a8470f514e1a799bf52d555da0
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-07-20 13:28:07 +0300

    add

---
+++ text_processing/example.py
@@ -0,0 +1,24 @@
+# How to print single '\':
+message = "C:\\path\\to\\file"
+print(message) # C:\path\to\file
+
+# How to print more than one quotation marks:
+print("I\'m happy")
+print("He said \"Hello!\"")
+
+# How to print words separated horizontally:
+message = "Hello\tWorld"
+print(message) # Hello   World
+
+# How to write multi-line string
+message = """This is randon text
+row 2 - test
+row 3 - test
+"""
+print(message) # prints the message in 3 lines
+
+# concatenation
+str1 = "Hello"
+str2 = "World"
+print(str1 + str2) # prints Hello World
+print(str1 * 10) # prints 10 times Hello
